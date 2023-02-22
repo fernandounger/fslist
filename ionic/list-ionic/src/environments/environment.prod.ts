144420b98ae7e353b2df069cedecd17d6fc295c8
Fernando Unger <fdasilvaunger@gmail.com>
2023-02-22 17:11:51 -0300

    feat: firebase configuration

--- ionic/list-ionic/src/environments/environment.prod.ts
+++ ionic/list-ionic/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBKcr05-zDiJV1mY-OE8s0gWTjJI-NCFPU",
+    authDomain: "task-manager-senac.firebaseapp.com",
+    projectId: "task-manager-senac",
+    storageBucket: "task-manager-senac.appspot.com",
+    messagingSenderId: "325902621879",
+    appId: "1:325902621879:web:5d0ab1b45f0dcb3e418132"
+  },
 };
--- ionic/list-ionic/src/environments/environment.ts
+++ ionic/list-ionic/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBKcr05-zDiJV1mY-OE8s0gWTjJI-NCFPU",
+    authDomain: "task-manager-senac.firebaseapp.com",
+    projectId: "task-manager-senac",
+    storageBucket: "task-manager-senac.appspot.com",
+    messagingSenderId: "325902621879",
+    appId: "1:325902621879:web:5d0ab1b45f0dcb3e418132"
+  },
 };
 
 /*
